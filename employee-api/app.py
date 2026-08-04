b45155f0e9a9d5addb3e7963b8b5a84d5245abe5
abiamahladi <abiamahladi@gmail.com>
2026-08-04 13:40:03 +0100

    feat(employee-api): add health checks and project automation

--- employee-api/app.py
+++ employee-api/app.py
@@ -110,6 +110,11 @@ def add_employee():
 
     return redirect(url_for("home"))
 
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    }), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
